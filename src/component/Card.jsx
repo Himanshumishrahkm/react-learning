52fed21bba4d667418bbd2a6416e21cc5ef9a04b
Himanshu Kumar <himanshumishrahkm0@gmail.com>
2026-05-25 22:43:38 +0530

    merging main and branch

---
+++ src/component/Card.jsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react'
+import {Bookmark} from 'lucide-react'
+
+
+
+function Card(props) {
+  const [saved, setSaved] = useState(false);
+  const str = localStorage();
+  return (
+    
+       
+        
+    
+      <div className="card">
+        <div className="top">
+          <img src={props.logo} alt="img" />
+          <button onClick={() => setSaved(!saved)}>save <Bookmark size={18}
+          fill={saved ? "black" : "none"}/></button>
+        </div>
+
+        <div className="center">
+          <h3>{props.company_name} <span>{props.timeline}</span></h3>
+          <h2>{props.position}</h2>
+          <div className='tag'>
+            <h4>{props.tag}</h4>
+            <h4>{props.level}</h4>
+          </div>
+
+        </div>
+
+        <div className="bottom">
+          <div>
+            <div>{props.cost}</div>
+            <div><h5>{props.location}</h5></div>
+          </div>
+          <button>Apply Now</button>
+        </div>
+
+      </div>
+       
+      
+      )
+    
+  
+}
+export default Card;
